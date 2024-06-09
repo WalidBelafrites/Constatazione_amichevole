0cfc1ec4537de590260a63ff9b3096a6a7585046
WalidBelafrites <walid.belafrites@gmail.com>
2024-06-09 12:27:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/constatazione_amichevole/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.constatazione_amichevole
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.constatazione_amichevole.ui.theme.Constatazione_amichevoleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Constatazione_amichevoleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Constatazione_amichevoleTheme {
+        Greeting("Android")
+    }
+}
