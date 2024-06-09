0a268de49ce9129b912057ae52d3cd61904b7649
WalidBelafrites <walid.belafrites@gmail.com>
2024-06-09 12:49:49 +0200

    inizio

--- app/src/main/java/com/example/constatazione_amichevole/MainActivity.kt
+++ app/src/main/java/com/example/constatazione_amichevole/MainActivity.kt
@@ -31,13 +31,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "A quest'app $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Constatazione_amichevoleTheme {
-        Greeting("Android")
+        Greeting("Benvenuti")
     }
 }
