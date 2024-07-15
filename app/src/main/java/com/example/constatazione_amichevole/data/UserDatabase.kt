5a0c6142067e526542b8a396bf32191bb4889e8b
Theo Connetable <theo.connetable@gmail.com>
2024-07-15 23:49:27 +0200

    Update UserDatabase.kt

--- app/src/main/java/com/example/constatazione_amichevole/data/UserDatabase.kt
+++ app/src/main/java/com/example/constatazione_amichevole/data/UserDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [Macchina::class],
+    entities = [Macchina::class, Constatazione::class],
     version = 1
 )
 
